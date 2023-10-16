4d19c60bfd0a1a196bc4d92563abfa5c300b7733
florallam <floralam73@gmail.com>
2023-10-17 00:53:18 +0800

    changes to week 2

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1>Flora's Tableau Learning</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/week2">Week 2</a>
+                <a href="/week3">Week 3</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
